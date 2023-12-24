183dc28e870602edb1a900f0c25cd00ec1f7153a
Sanikaaa33 <126164345+Sanikaaa33@users.noreply.github.com>
2023-12-24 22:00:43 +0530

    Add files via upload

---
+++ CodSoft3.java
@@ -0,0 +1,143 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student implements Serializable {
+    private String name;
+    private int rollNumber;
+    private char grade;
+
+    public Student(String name, int rollNumber, char grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public char getGrade() {
+        return grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Roll Number: " + rollNumber + ", Grade: " + grade;
+    }
+}
+
+class StudentManagementSystem {
+    private ArrayList<Student> students;
+    private static final String FILE_NAME = "students.dat";
+
+    public StudentManagementSystem() {
+        students = new ArrayList<>();
+        loadStudentsFromFile();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+        saveStudentsToFile();
+    }
+
+    public void removeStudent(int rollNumber) {
+        students.removeIf(student -> student.getRollNumber() == rollNumber);
+        saveStudentsToFile();
+    }
+
+    public Student searchStudent(int rollNumber) {
+        for (Student student : students) {
+            if (student.getRollNumber() == rollNumber) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public void displayAllStudents() {
+        for (Student student : students) {
+            System.out.println(student);
+        }
+    }
+
+    private void loadStudentsFromFile() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
+            students = (ArrayList<Student>) ois.readObject();
+        } catch (FileNotFoundException e) {
+            System.out.println("No existing data file found. Starting with an empty student list.");
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveStudentsToFile() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            oos.writeObject(students);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class CodSoft3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StudentManagementSystem studentManagementSystem = new StudentManagementSystem();
+
+        while (true) {
+            System.out.println("Student Management System");
+            System.out.println("1. Add a new student");
+            System.out.println("2. Remove a student");
+            System.out.println("3. Search for a student");
+            System.out.println("4. Display all students");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter student name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter roll number: ");
+                    int rollNumber = scanner.nextInt();
+                    System.out.print("Enter grade: ");
+                    char grade = scanner.next().charAt(0);
+                    Student newStudent = new Student(name, rollNumber, grade);
+                    studentManagementSystem.addStudent(newStudent);
+                    break;
+                case 2:
+                    System.out.print("Enter roll number to remove: ");
+                    int rollToRemove = scanner.nextInt();
+                    studentManagementSystem.removeStudent(rollToRemove);
+                    break;
+                case 3:
+                    System.out.print("Enter roll number to search: ");
+                    int rollToSearch = scanner.nextInt();
+                    Student foundStudent = studentManagementSystem.searchStudent(rollToSearch);
+                    if (foundStudent != null) {
+                        System.out.println("Student found: " + foundStudent);
+                    } else {
+                        System.out.println("Student not found.");
+                    }
+                    break;
+                case 4:
+                    System.out.println("All Students:");
+                    studentManagementSystem.displayAllStudents();
+                    break;
+                case 5:
+                    System.out.println("Exiting application.");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 5.");
+            }
+        }
+    }
+}
